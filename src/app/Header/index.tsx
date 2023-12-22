49a8458568bd529dc3b17d0bd246545cf176eb23
Abbou Najat <najatabb110@gmail.com>
2023-12-22 10:51:10 +0100

    modification

--- src/app/Header/index.tsx
+++ src/app/Header/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 import { FiMail, FiPhone, FiMapPin } from 'react-icons/fi';
-import styles from '@/app/ui/separateur.module.css';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+
+
+
+
+
 
 const Header = () => {
   const name = 'ABBOU Najat';
@@ -12,27 +19,29 @@ const Header = () => {
 
   return (
     <header>
-      <div className='flex bg-orange-900 mb-10 items-center'>
-      {/* overflow-visible  */}
+      <div className='flex bg-slate-600 mb-10 items-center'>
       <div className="h-48 w-48 rounded-full overflow-hidden">
         <Image className="object-cover h-full w-full" src="/profile.jpg" alt="Ma photo de profil" width={500}  height={512} />
       </div>
-      <div className='flex-grow flex items-center justify-center text-center'>
-        <div >
-          <h1 className='text-4xl uppercase my-2.5 mt-10'>{name}</h1>
-          <p className='text-2xl uppercase mb-20'>{title}</p>
-        </div>
+      <div className='flex-grow flex items-center justify-center text-center text-gray-50'>
+      <div>
+        <h1 className='text-4xl uppercase my-2.5 mt-10'>{name}</h1>
+        <p className='text-2xl uppercase mb-10'>{title}</p>
+        <ul className='ml-5 text-stone-100'>
+          <li><FiMail className="inline-block align-text-bottom text-pink-950" /> {email}</li>
+          <li><FiPhone className="inline-block align-text-bottom text-pink-950" /> {phone}</li>
+          <li className='mb-1'><FiMapPin className="inline-block align-text-bottom text-pink-950" /> {address}</li>
+          {/* <li className='max-w-sm mx-auto  shadow-lg flex items-center'><a href="URL_DE_VOTRE_GITHUB_PROFILE"> */}
+       <li className='flex items-center justify-center text-center'> <FontAwesomeIcon icon={faGithub} className='h-5 w-5 text-pink-950 mr-1' />
+      {/* </a> */}
+      <Link href="https://github.com/Najattt/Cv_nextLS/">
+      @Najat
+      </Link>
+          </li>  
+        </ul>
       </div>
     </div>
-      <div>
-        <h1 className='text-xl uppercase mb-4' >Contact</h1>
-        <hr className={styles.separator} />
-      <ul className='ml-5 text-yellow-700'>
-      <li><FiMail className="inline-block align-text-bottom text-amber-900" /> {email}</li>
-      <li><FiPhone className="inline-block align-text-bottom text-amber-900" /> {phone}</li>
-      <li className='mb-10'><FiMapPin className="inline-block align-text-bottom text-amber-900 " /> {address}</li>
-      </ul>
-      </div>
+    </div>
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,12 @@ import Education from './Education';
 import Skills from './Skills';
 import Experiences from './Experience';
 import Loisirs from './Loisirs';
-const separatorStyle = {
-  border: 'none',
-  height: '2px', 
-  backgroundColor: '#ccc', 
-  margin: '20px 0', }
+
 
 const Home = () => {
   return (
     <div className='mb-40'>
       <div className='ml-20 mr-20'><Header /></div>
-      
-      {/* <hr style={separatorStyle} /> */}
       <div className='mr-20'><Education/></div>
       <div className='ml-20'><Skills /></div>
        <div className='ml-20'><Experiences /></div> 
